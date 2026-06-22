18135d61c9ca586bdfdca2137b70f7c977ad0726
SaraAlbino <albino.sara@gmail.com>
2026-06-22 23:54:10 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ const MICHELLE_CONFIG = {
   owner: "SaraAlbino",
   repo:  "michelle",
   branch: "main",
-  pat:   "COLOCA_AQUI_O_PAT_READ_ONLY"
+  pat:   "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"
 };
